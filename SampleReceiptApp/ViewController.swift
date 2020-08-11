84c9e5b280a47c5e6ffecfd3da48e8b6da6ef978
Shunzhe <git@mszmagic.com>
2020-08-11 15:19:22 -0400

    Initial Commit

---
+++ SampleReceiptApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleReceiptApp
+//
+//  Created by Shunzhe Ma on 8/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
